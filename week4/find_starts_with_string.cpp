ec2e9b3de26ffa10e53303903ac97e2fc330f3c1
mmarti <mmarti@student.21-school.ru>
2020-06-05 18:47:04 +0300

    chore

---
+++ week4/find_starts_with_string.cpp
@@ -0,0 +1,38 @@
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(
+		RandomIt range_begin, RandomIt range_end, const string& prefix) {
+	string s = prefix;
+	s[s.size() - 1] += 1;
+	return {lower_bound(range_begin, range_end, prefix),
+		  lower_bound(range_begin, range_end, s)};
+}
+
+int main() {
+	const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+
+	const auto mo_result =
+			FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+	for (auto it = mo_result.first; it != mo_result.second; ++it) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	const auto mt_result =
+			FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+	cout << (mt_result.first - begin(sorted_strings)) << " " <<
+		 (mt_result.second - begin(sorted_strings)) << endl;
+
+	const auto na_result =
+			FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+	cout << (na_result.first - begin(sorted_strings)) << " " <<
+		 (na_result.second - begin(sorted_strings)) << endl;
+
+	return 0;
+}
